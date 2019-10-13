76ff8ca9a492aadbd0d4d18709bad1261b74d790
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-13 18:30:01 +0900

    style post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
