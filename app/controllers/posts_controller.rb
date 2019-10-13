444e09cf761d779fbbb6dd3859ca21828159776a
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-13 19:16:55 +0900

    add random post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ before_action :authenticate_user!, except: [:index, :show]
 
   def show
     @comments = Comment.where(post_id: @post)
+    @rand_post = Post.where.not(id: @post).order("RAND()").first
   end
 
   def new
